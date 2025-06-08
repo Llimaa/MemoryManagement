96357b68f860a82ea26ac32428e6ec2a1f6b2227
Marcos Limal <marcoslimadefatima@gmail.com>
2025-06-07 21:07:52 -0300

    feat: example stack and heap memory in C#

---
+++ Heap/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var people = new People();
+        people.Name = "Maria";
+        people.Age = 30;
+
+        Console.WriteLine($"Name: {people.Name}, Age: {people.Age}");
+    }
+}
+
+public class People
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+// Comportamentos:
+// 1. new People() -> cria um objeto na memória heap.
+// 2. a var people -> cria um ponteiro(referência) na memória stack, apontando para a posição com os dados na memória heap.
+// 3. people.Name = "Maria" e people.Age = 30 atribuem valores aos campos do objeto na heap.
+// 4. Console.WriteLine(...) acessa os valores através da referência people, buscando os dados na heap.
+// 3. Após o fim do escopo (Main), o ponteiro people é removida da Stack
+// 4. O objeto na Heap se torna elegível para coleta pelo Garbage Collector
+
+// +--------------------------------+
+// |            Stack               |
+// +--------------------------------+
+// | people --> (referência) ---+   |
+// |                            |   |
+// +----------------------------|---+
+//                              |
+//                              v
+//                  +-----------------------+
+//                  |        Heap           |
+//                  +-----------------------+
+//                  | Objeto People         |
+//                  | Nome = "Maria"        |
+//                  | Idade = 30            |
+//                  +-----------------------+
---
+++ Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 10;
+        int y = 20;
+
+        Sum(x, y);
+    }
+
+    static void Sum(int param1, int param2)
+    {
+        int result = param1 + param2;
+        ShowResult(result);
+    }
+
+    static void ShowResult(int result) =>
+        Console.WriteLine($"The result is: {result}");
+}
+
+// Como fica a alocação dos recursos em tempo de execução.
+// Cada bloco é um stack frame, e note que o primeiro a ser chamado foi o metodo main, mas ele fica no fim da nossa pilha,
+// pois quando o sistema for liberar os recursos, o main será o ultimo a ser liberado da mémoria.
+// Quando chamamos uma função que tem parâmetros, a primeira coisa a ser alocada no Stack Frame são os parâmetros passados,
+// logo após é gravado o endereço para o onde o método deve retornar ao finalizar sua execução 
+// e por fim as suas variáveis locais.
+
+// [Stack]
+// -------------
+// | ShowResult |
+// | result = 30|
+// -------------
+// | Sum        |
+// | result = 30|
+// | Return Main|
+// | param2 = 20|
+// | param1 = 10|
+// -------------
+// | Main       |
+// | x = 10     |
+// | y = 20     |
+// -------------
